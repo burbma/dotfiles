3d58e6fa15f2753723de74485c6b57c05359445d
Matt Burbidge <burbma@gmail.com>
2021-09-25 12:36:55 -0600

    remove dnd

--- bash/dnd-off.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -x
-
-defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui dndEnd -integer $(($(date +"%H") * 60 + $(date +"%M") + 1))
-killall NotificationCenter
--- bash/dnd-on.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-
-defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui dndStart -integer $(($(date +"%H") * 60 + $(date +"%M") + 1))
-killall NotificationCenter
-
