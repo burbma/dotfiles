375ce60329ff896691927b16f5b850c2ed33b558
Matt Burbidge <burbma@gmail.com>
2022-07-29 15:48:14 -0600

    rework prompt for line wrapping

--- bash/basic-git-prompt.sh
+++ bash/basic-git-prompt.sh
@@ -1,5 +1,15 @@
 # Requires ~/dotfiles/bash/bash_colors to be sourced already.
 
+txtrst='\[\033[0m\]'       # Text Reset
+txtblk='\[\033[0;30m\]'        # Black
+txtred='\[\033[0;31m\]'          # Red
+txtgrn='\[\033[0;32m\]'        # Green
+txtylw='\[\033[0;33m\]'       # Yellow
+txtblu='\[\033[0;34m\]'         # Blue
+txtpur='\[\033[0;35m\]'       # Purple
+txtcyn='\[\033[0;36m\]'         # Cyan
+txtwht='\[\033[0;37m\]'        # White
+
 function git_prompt() {
   if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
     branch_name=$(git symbolic-ref -q HEAD)
@@ -29,7 +39,7 @@ function git_prompt() {
     fi
 
     if git status 2> /dev/null | grep -q 'Untracked files'; then
-      echo -n "${txtcyn}%"
+      echo -n "${txtpur}%"
     fi
 
     if git stash list 2> /dev/null | grep -q 'stash'; then
